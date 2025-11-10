96145982fc290dbf5ba93a8a771b3cad3f3fdf54
Mika Vilpas <mika.vilpas@gmail.com>
2025-11-10 17:18:40 +0200

    chore(renovate): keep flatten.nvim up to date for tests (#418)

--- integration-tests/test-environment/.config/nvim/lua/plugins.lua
+++ integration-tests/test-environment/.config/nvim/lua/plugins.lua
@@ -15,8 +15,8 @@ local plugins = {
   {
     -- Open files and command output from wezterm, kitty, and neovim terminals in
     -- your current neovim instance
-    -- https://github.com/willothy/flatten.nvim
-    "willothy/flatten.nvim",
+    "https://github.com/willothy/flatten.nvim",
+    -- renovate: datasource=git-refs packageName=https://github.com/willothy/flatten.nvim
     commit = "7252779",
     -- Ensure that it runs first to minimize delay when opening file from terminal
     lazy = false,
